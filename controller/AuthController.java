63aefab93bc0cf741933d381385c94c9090eb27f
7000ayush <162450731+7000ayush@users.noreply.github.com>
2024-11-10 03:08:06 +1300

    Add files via upload

---
+++ controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.hma.hotel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hma.hotel.dto.LoginRequest;
+import com.hma.hotel.dto.Response;
+import com.hma.hotel.entity.User;
+import com.hma.hotel.service.interfac.IUserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+@Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+
+}
